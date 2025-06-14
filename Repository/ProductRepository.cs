f821c57bf5f86bd1e614f9f407ae96057b854155
Arcturus22 <amitsharma89460@gmail.com>
2025-06-15 00:13:44 +0530

    Refined the Register and Login pages

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -7,9 +7,11 @@ namespace SwiftCart.Repository
     public class ProductRepository : IProductRepository
     {
         private readonly ApplicationDbContext _db;
-        public ProductRepository(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductRepository(ApplicationDbContext db , IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<Product> CreateAsync(Product obj)
@@ -39,6 +41,11 @@ namespace SwiftCart.Repository
         {
          
             var obj = await _db.Product.FirstOrDefaultAsync(c => c.Id == id);
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('/'));
+            if(File.Exists(imagePath))
+            {
+                File.Delete(imagePath); // Delete the image file if it exists
+            }
             if (obj != null){
                 _db.Product.Remove(obj);
                 return (await _db.SaveChangesAsync()) > 0;
---
+++ Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SwiftCart.Utility
+{
+    public static class SD 
+    {
+        // SD = Static Details or Static Data
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+
+        public static readonly IEnumerable<string> Roles = new[] { Role_Admin, Role_Customer };
+
+    }
+}
