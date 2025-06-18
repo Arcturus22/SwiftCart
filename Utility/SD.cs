513ff722d6edc016c1ff107e123f3f9cda49bdea
Arcturus22 <amitsharma89460@gmail.com>
2025-06-18 14:31:16 +0530

    Order Creation Completed

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using SwiftCart.Data;
+using System.Runtime.CompilerServices;
 
 namespace SwiftCart.Utility
 {
@@ -8,7 +9,32 @@ namespace SwiftCart.Utility
         public static string Role_Admin = "Admin";
         public static string Role_Customer = "Customer";
 
-        public static readonly IEnumerable<string> Roles = new[] { Role_Admin, Role_Customer };
+        //public static readonly IEnumerable<string> Roles = new[] { Role_Admin, Role_Customer };
 
+        public static string StatusPending = "Pending";
+        public static string StatusReadyForPickUp = "ReadyForPickUp ";
+        public static string StatusCompleted= "Completed";
+        public static string StatusCancelled = "Cancelled";
+
+        // Conversion method that will convert list of shopping cart to list of order details
+        public static List<OrderDetail> ConvertShoppingCartToOrderDetails(List<ShoppingCart> shoppingCarts)
+        {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+
+            foreach(var cart in shoppingCarts)
+            {
+                OrderDetail orderDetail = new OrderDetail()
+                {
+                    ProductId = cart.ProductId,
+                    Count = cart.Count,
+                    Price = Convert.ToDouble(cart.Product.Price),
+                    ProductName = cart.Product.Name
+
+                };
+                orderDetails.Add(orderDetail);
+            }
+
+            return orderDetails;
+        }
     }
 }
